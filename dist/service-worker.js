9a065ddd9e48cc6b8ca4ee3f7c0570d3a9824518
aatishgyawali <aatishgyawali10@gmail.com>
2023-03-04 17:53:14 +0000

    Adding dist files

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,7 +1,9 @@
-let cacheName = "store-c2";
+let cacheName = "store-c3";
 let cacheFiles = [
   "index.html",
-  "assets",
+  "src/App.vue",
+  "src/components/Checkout.vue",
+  "src/components/Lesson.vue",
   "images/maths.png",
   "images/computer.png",
   "images/eng.png",
@@ -10,26 +12,16 @@ let cacheFiles = [
   "images/pound.png",
   "images/sports.png",
 ];
+
 self.addEventListener("install", function (e) {
   console.log("[Service Worker] Install");
   e.waitUntil(
       caches.open(cacheName).then(function (cache) {
         console.log("[Service Worker] Caching files");
-
-        cacheFiles.forEach(cacheFile=>{
-          try{
-
-            cache.add(cacheFile);
-          }
-          catch{
-            console.log(cacheFile);
-          }
-        })
-        return cache;
+        return cache.addAll(cacheFiles);
       })
   );
 });
-
 self.addEventListener("fetch", function (e) {
   e.respondWith(
       caches.match(e.request).then(function (cachedFile) {
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,9 @@
-let cacheName = "store-c2";
+let cacheName = "store-c3";
 let cacheFiles = [
   "index.html",
-  "assets",
+  "src/App.vue",
+  "src/components/Checkout.vue",
+  "src/components/Lesson.vue",
   "images/maths.png",
   "images/computer.png",
   "images/eng.png",
@@ -10,49 +12,39 @@ let cacheFiles = [
   "images/pound.png",
   "images/sports.png",
 ];
+
 self.addEventListener("install", function (e) {
   console.log("[Service Worker] Install");
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      console.log("[Service Worker] Caching files");
-
-      cacheFiles.forEach(cacheFile=>{
-        try{
-
-        cache.add(cacheFile);
-        }
-        catch{
-          console.log(cacheFile);
-        }
+      caches.open(cacheName).then(function (cache) {
+        console.log("[Service Worker] Caching files");
+        return cache.addAll(cacheFiles);
       })
-      return cache;
-    })
   );
 });
-
 self.addEventListener("fetch", function (e) {
   e.respondWith(
-    caches.match(e.request).then(function (cachedFile) {
-      //return the file if it is not in the cache
-      if (cachedFile) {
-        console.log(
-          "[Service Worker] Resource fetched from the cache for: " +
-            e.request.url
-        );
-        return cachedFile;
-      } else {
-        return fetch(e.request).then(function (response) {
-          return caches.open(cacheName).then(function (cache) {
-            //add the new file to the cache
-            cache.put(e.request, response.clone());
-            console.log(
-              "[Service Worker] Resource fetched and saved in the cache for: " +
-                e.request.url
-            );
-            return response;
+      caches.match(e.request).then(function (cachedFile) {
+        //return the file if it is not in the cache
+        if (cachedFile) {
+          console.log(
+              "[Service Worker] Resource fetched from the cache for: " +
+              e.request.url
+          );
+          return cachedFile;
+        } else {
+          return fetch(e.request).then(function (response) {
+            return caches.open(cacheName).then(function (cache) {
+              //add the new file to the cache
+              cache.put(e.request, response.clone());
+              console.log(
+                  "[Service Worker] Resource fetched and saved in the cache for: " +
+                  e.request.url
+              );
+              return response;
+            });
           });
-        });
-      }
-    })
+        }
+      })
   );
 });
